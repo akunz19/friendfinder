cd20c3db32356db67ceffc22e5dbd6038d281d10
amanda <amanda.m.kunz@gmail.com>
2020-01-09 09:43:14 -0600

    adding changes, still need to fix survey required fields

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8070;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+require("./routing/api-routes")(app);
+require("./routing/html-routes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
