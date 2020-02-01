e254581b6264103495796b6b6d4bbb240b0d482c
amanda <amanda.m.kunz@gmail.com>
2020-02-01 12:22:56 -0600

    updating logic

--- app/data/bachelors.js
+++ app/data/bachelors.js
@@ -1,48 +1,48 @@
-var match;
+
 var Bachelors = [{
     name: "Josh",
     image: "https://cdn.pixabay.com/photo/2014/11/21/12/13/man-540500_960_720.jpg",
     scores: [5,3,2,1,4,5,2,3,2,5],
-    match: match
+    
 }, {
     name: "Brandon",
     image: "https://cdn.pixabay.com/photo/2018/06/26/00/01/entrepreneurship-3498255_960_720.jpg",
     scores: [3,1,2,1,5,5,4,3,2,1],
-    match: match
+    
 },
 {
     name: "Chad",
     image: "https://cdn.pixabay.com/photo/2016/12/06/17/50/male-1887117_960_720.jpg",
     scores: [2,1,4,3,5,2,1,2,4,5],
-    match: match
+    
 
 },
 {
     name: "Staci",
     image: "https://cdn.pixabay.com/photo/2018/01/25/14/12/nature-3106213_960_720.jpg",
     scores: [1,5,3,2,4,5,5,4,5,5],
-    match: match
+    
 
 },
 {
     name: "Becky",
     image: "https://cdn.pixabay.com/photo/2016/01/19/17/48/woman-1149911_960_720.jpg",
     scores: [1,2,3,4,4,5,3,4,5,5],
-    match: match
+    
 
 },
 {
     name: "Christie",
     image: "https://cdn.pixabay.com/photo/2017/03/17/04/07/beautiful-2150881_960_720.jpg",
     scores: [4,2,2,4,3,5,3,4,5,5],
-    match: match
+    
 
 },
 {
     name: "Chris Harrison",
     image: "https://jk0uz2gof2j3u6upx2ef89l6-wpengine.netdna-ssl.com/wp-content/uploads/2019/04/45E44B48-A8E0-4A04-B73A-8A47F51E235B-683x1024.jpeg",
     scores: [5,5,5,5,5,5,5,5,5,5],
-    match: match
+    
 
 }
 ];
--- routing/api-routes.js
+++ routing/api-routes.js
@@ -6,37 +6,48 @@ module.exports = function(app) {
   });
 
   app.post("/api/bachelors", function(req, res) {
-    var newBachelor = req.body;
-    var diffArray = [];
-    var index;
-    function getDifference() {
-      for (var i = 0; i < Bachelors.length; i++) {
-        var diffNumber = []; // holds the difference of scores
-        for (var j = 0; j < Bachelors[i].scores.length; j++) {
-          var difference = Math.abs(
-            newBachelor.scores[j] - Bachelors[i].scores[j] // grabs the differences of the survey scores
-          );
-          diffNumber.push(difference); //pushes differenes of each score to diffarray
-        }
-        console.log(Bachelors[i]);
-        function getSum(total, num) { //gets the sum of the array values
-          return total + num;
+    console.log(req.body);
+    if (
+      req.body.name != "" &&
+      req.body.img != "" &&
+      !req.body.scores.includes(NaN)
+    ) {
+      var newBachelor = req.body;
+      var diffArray = [];
+      var index;
+      function getDifference() {
+        for (var i = 0; i < Bachelors.length; i++) {
+          var diffNumber = []; // holds the difference of scores
+          for (var j = 0; j < Bachelors[i].scores.length; j++) {
+            var difference = Math.abs(
+              newBachelor.scores[j] - Bachelors[i].scores[j] // grabs the differences of the survey scores
+            );
+            diffNumber.push(difference); //pushes differenes of each score to diffarray
+          };
+          var diffSum = diffNumber.reduce(getSum); //calculates the total difference
+          diffArray.push(diffSum); //pushes total difference score for each user to array
+          console.log("diffsum", diffSum, "diffArray", diffArray);
         };
-        var diffSum = diffNumber.reduce(getSum); //calculates the total difference
-        diffArray.push(diffSum); //pushes total difference score for each user to array
-        console.log("diffsum", diffSum, "diffArray", diffArray);
-      }
-      var min = Math.min(...diffArray); //grabs the lowest score
-      console.log(min);
-      index = diffArray.indexOf(min); //gets the index of the lowest score
-      console.log("index", index);
-    }
-    getDifference();
-    var match = Bachelors[index]; //assigns the match
-    console.log("YOUR MATCH IS!!!", match);
-    req.body.match = match;
-    Bachelors.push(req.body);
-    res.json(true);
-    console.log("req.body",req.body.match);
+        var min = Math.min(...diffArray); //grabs the lowest score
+        console.log(min);
+        index = diffArray.indexOf(min); //gets the index of the lowest score
+        console.log("index", index);
+      };
+      getDifference();
+      var match = Bachelors[index]; //assigns the match
+      console.log("YOUR MATCH IS!!!", match);
+      req.body.match = match;
+      Bachelors.push(req.body);
+      res.json(true);
+      console.log("req.body", req.body.match);
+    } else {
+      res.json(false);
+    };
   });
+
+  function getSum(total, num) {
+    //gets the sum of the array values
+    return total + num;
+  }
 };
+
